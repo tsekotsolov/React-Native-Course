b758d2fd78a07856ee633c5248a8bb408771b82d
Tseko Tsolov <tsekotsolov@gmail.com>
2018-12-28 18:15:26 +0200

    enchancers added

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,17 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, compose} from 'redux'
 import placesReducer from './reducers/placesReducer'
 
 const rootReduser = combineReducers({
   places: placesReducer
 })
 
+let composeEnchancers = compose
+if (__DEV__) {
+  composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const configureStore = () => {
-  return createStore(rootReduser)
+  return createStore(rootReduser, composeEnchancers())
 }
 
 export default configureStore
