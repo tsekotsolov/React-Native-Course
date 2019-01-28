a5e1b2c4fa1570997d803e1ac6582dc525130b8f
Tseko Tsolov <tseko.tsolov@ad.mentormate.bg>
2019-01-28 20:04:38 +0200

    Markers and map animations added

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -4,37 +4,90 @@ import styled from 'styled-components'
 import MapView from 'react-native-maps'
 
 class PickLocation extends React.Component {
-
   state = {
     focusedLocation: {
       latitude: 37.7900352,
       longitude: -122.4013726,
       latitudeDelta: 0.0122,
-      longitudeDelta: Dimensions.get('window').width / Dimensions.get('window').height * 0.0122
-    }
+      longitudeDelta:
+        Dimensions.get('window').width /
+        Dimensions.get('window').height *
+        0.0122
+    },
+    locationChosen: false
   }
+
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent
+
+    this.map.animateToRegion({
+      ...this.state.focusedLocation,
+      latitude: coords.coordinate.latitude,
+      longitude: coords.coordinate.longitude,
+    })
+
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.coordinate.latitude,
+          longitude: coords.coordinate.longitude,
+        },
+        locationChosen: true
+      }
+    })
+
+    this.props.locationPickHandler({
+      latitude: coords.coordinate.latitude,
+      longitude: coords.coordinate.longitude,
+    })
+  }
+
+  getLocationHandler = _ => {
+    navigator.geolocation.getCurrentPosition(pos => {
+
+      const coordsEvent = {
+        nativeEvent: {
+          coordinate: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+          },
+        }
+      }
+
+      this.pickLocationHandler(coordsEvent)
+    }, err => {
+      console.log(err)
+      alert('Fetching the position failed! Pick one manually!')
+    })
+  }
+
   render() {
+    let marker = null
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />
+    }
     return (
       <MainContainer>
-        <MapView
+        <Map
           initialRegion={this.state.focusedLocation}
-          style={styles.map}
-        />
+          onPress={this.pickLocationHandler}
+          ref={ref => { this.map = ref }}
+        >
+          {marker}
+        </Map>
         <ButtonView>
-          <Button title='Locate Me' onPress={() => alert('Pick Location')} />
+          <Button title="Locate Me" onPress={this.getLocationHandler} />
         </ButtonView>
       </MainContainer>
     )
   }
 }
 
-
-const styles = StyleSheet.create({
-  map: {
-    width: '100%',
-    height: 250
-  }
-})
+const Map = styled(MapView)`
+  width: 100%;
+  height: 250px;
+`
 
 const MainContainer = styled.View`
   width: 100%;
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, Button } from 'react-native'
 import { connect } from 'react-redux'
-import PlaceInput from '../../components/PlaceInput/PlaceInput.js'
 import { addPlace } from '../../store/actions/index'
 import styled from 'styled-components'
 import MainText from '../../components/UI/MainText/MainText'
 import HeadingText from '../../components/UI/HeadingText/HeadingText'
 import PickImage from '../../components/PickImage/PickImage'
 import PickLocation from '../../components/PickLocation/PickLocation'
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
 
 class SharePlaceScreen extends Component {
   static navigatorStyle = {
@@ -17,6 +17,14 @@ class SharePlaceScreen extends Component {
   constructor(props) {
     super(props)
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent)
+  }
+
+  state = {
+    location: {
+      value: null,
+      isValid: false
+    },
+    place: ''
   }
 
   onNavigatorEvent = event => {
@@ -29,8 +37,29 @@ class SharePlaceScreen extends Component {
     }
   }
 
-  placesAddedHandler = placeName => {
+  placesAddedHandler = (placeName) => {
+
+    console.log(placeName)
+    console.log(this.state.location)
     this.props.onAddPlace(placeName)
+  }
+
+  locationPickHandler = location => {
+    this.setState(prevState => {
+      return {
+        location: {
+          ...prevState.location,
+          value: location,
+          isValid: true
+        }
+      }
+    })
+  }
+
+  onChangeInputHandler = value => {
+    this.setState({
+      place: value
+    })
   }
 
   render() {
@@ -41,8 +70,25 @@ class SharePlaceScreen extends Component {
             <HeadingText>Share a place with us!</HeadingText>
           </MainText>
           <PickImage />
-          <PickLocation />
-          <PlaceInput addPlaces={this.placesAddedHandler} />
+          <PickLocation locationPickHandler={this.locationPickHandler} />
+          <InputContainer>
+            <DefaultInput
+              placeholder="Add awesome place..."
+              onChangeText={this.onChangeInputHandler}
+              value={this.state.place}
+              isValid={true}
+            />
+            <Button
+              title="Add Place"
+              disabled={this.state.place.length < 3 || !this.state.location.isValid}
+              onPress={_ => {
+                this.state.place !== '' && this.placesAddedHandler(this.state.place)
+                this.setState({
+                  place: ''
+                })
+              }}
+            />
+          </InputContainer>
         </Container>
       </ScrollView>
     )
@@ -58,6 +104,18 @@ const mapDispatchToProps = dispatch => {
 const Container = styled.View`
   flex: 1;
   align-items: center;
+`
+
+const InputContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 70px;
+`
+
+const StyledInput = styled.TextInput`
+  height: 40px;
+  width: 80%;
 `
 
 export default connect(null, mapDispatchToProps)(SharePlaceScreen)
