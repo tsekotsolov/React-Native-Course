d9ccdb9ffd463338620eb0c815e9ee972f4c5eb4
Tseko Tsolov <tseko.tsolov@ad.mentormate.bg>
2019-01-31 13:14:44 +0200

    Sign up to firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,34 @@
 import { TRY_AUTH } from './actionTypes'
 
 export const tryAuth = authData => {
-  return {
-    type: TRY_AUTH,
-    payload: authData
+  return dispatch => {
+    dispatch(authSignup(authData))
+  }
+}
+
+export const authSignup = authData => {
+  return dispatch => {
+    fetch(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDz-ESTzaQdVu3hPir9KuAlsRImSWSMcjc',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .catch(err => {
+        console.log(err)
+        alert('Authentication failed! Try again!')
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes)
+      })
   }
 }
